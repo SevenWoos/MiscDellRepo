ae55244a22a52c85ef6df7c2d91e02ba2bec345e
SevenWoos <swenwu96@gmail.com>
2026-01-29 13:20:49 -0800

    updated keystroke events

--- RockPaperScissors/mark3/scripts/mark3.js
+++ RockPaperScissors/mark3/scripts/mark3.js
@@ -168,3 +168,18 @@ document.querySelector('.js-reset-score-button')
   .addEventListener('click', () => {
     showResetConfirmation();
 });
+
+
+document.body.addEventListener('keydown', (event) => {
+  if (event.key === 'r') {
+    playGame('rock');
+  } else if (event.key === 'p') {
+    playGame('paper');
+  } else if (event.key === 's') {
+    playGame('scissors');
+  } else if (event.key === 'a') {
+    auto_play();
+  } else if (event.key === 'Backspace') {
+    showResetConfirmation();
+  }
+});
